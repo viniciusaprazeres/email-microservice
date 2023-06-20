58d6db26283fc6d99ddae6e3dcf508eabf968998
Vinicius Prazeres <viniciusaprazeres@gmail.com>
2023-06-19 22:04:48 -0300

    FEAT: Create EmailModel

---
+++ src/main/java/com/viniciusaprazeres/email/model/EmailModel.java
@@ -0,0 +1,27 @@
+package com.viniciusaprazeres.email.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long emailId;
+    private String ownerReference;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+
+}
