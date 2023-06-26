cf1e27bf1c5628f2ac3c4c2dff3d9add5b3afa08
Vinicius Prazeres <viniciusaprazeres@gmail.com>
2023-06-26 20:20:13 -0300

    FIX: Change the type for the id

--- src/main/java/com/viniciusaprazeres/email/repository/EmailRepository.java
+++ src/main/java/com/viniciusaprazeres/email/repository/EmailRepository.java
@@ -3,6 +3,8 @@ package com.viniciusaprazeres.email.repository;
 import com.viniciusaprazeres.email.model.EmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailRepository extends JpaRepository<EmailModel, Long> {
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
 
 }
